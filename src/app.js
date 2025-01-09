6b7e42fe1318fe6f90600bab2f0251479760d4a2
Leandro Amaral <lramaral02m@gmail.com>
2025-01-09 11:44:42 -0300

    rafatorando rotas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import conectaNoDatabase from "./config/dbConnect.js";
-import livro from "./models/livro.js";
+import routes from "./routes/index.js";
 
 const conexao = await conectaNoDatabase();
 
@@ -13,11 +13,7 @@ conexao.once("open", () => {
 });
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).send("Curso de Node.js");
-});
+routes(app);
 
 app.get("/livros/:id", (req, res) => {
   const index = buscarLivro(req.params.id);
